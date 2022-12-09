e98830a2fde161f2bcd613fe68753ea330acaa71
naren-dev7676 <118735103+naren-dev7676@users.noreply.github.com>
2022-12-09 12:59:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# github-to-ec2-cicd
+# github-to-lambda-demo
---
+++ lamba_function.py
@@ -0,0 +1,9 @@
+print("Welcome to CICD Demo")
+print('welcome to sonarqube demo1')
+# print('welcome to sonarqube demo1')
+
+print("Hi,this is CICD DEMO1")
+print("Hi,this is CICD DEMO2")
+
+
+print("git change sucessfully cloned to ec2 instance")
